3d9ad1a0369a8186a88ce088ef83032d3131d3c5
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-09-25 15:54:13 +0200

    sistemazioni ulteriori

--- Progetto/js/footer.js
+++ Progetto/js/footer.js
@@ -48,7 +48,7 @@ function Footer() {
                             <span>Seguici</span>
                             <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>
                             <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
-                            <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                            <a href="#"><i class="fab fa-instagram instagram-bg"></i></a>
                         </div>
                     </div>
                 </div>
