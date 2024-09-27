be6c83f718377226894ba98815c69b53f5a3fd5b
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-09-27 12:55:48 +0200

    login

--- Progetto/js/homepagejs.js
+++ Progetto/js/homepagejs.js
@@ -58,8 +58,10 @@ function navbar() {
     </ul>
     <!-- Login/Signup -->
     <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-3">
-    <a href="./login.html" class="text-white text-decoration-none"><i class="fa-solid fa-arrow-right-to-bracket"></i> &nbsp;Login </a>
-    <a href="./registrati.html" class="text-black text-decoration-none px-3 py-1 rounded-4" style="background-color: coral;">Sign Up&nbsp; <i class="fa-solid fa-pen"></i></a>
+    <a href="./login.html" class="text-white text-decoration-none d-none" class="user"><i class="fa-solid fa-user"></i> &nbsp;User </a>
+    <a href="login.html" class="text-white text-decoration-none" class="login"><i class="fa-solid fa-arrow-right-to-bracket"></i> &nbsp;Login </a>
+    <a href="login.html" class="text-white text-decoration-none d-none" class="logout"><i class="fa-solid fa-arrow-right-from-bracket"></i> &nbsp;Logout </a>
+    <a href="registrati.html" class="text-black text-decoration-none px-3 py-1 rounded-4" class="signup" style="background-color: coral;">Sign Up&nbsp; <i class="fa-solid fa-pen"></i></a>
     </div>
     
     </div>
@@ -95,3 +97,137 @@ chiusura.addEventListener("click", function () {
     dropdown2.classList.remove("show");
 });
 
+
+let user = document.querySelector('.user');
+let logout = document.querySelector('.logout');
+let login = document.querySelector('.login');
+let signup = document.querySelector('.signup');
+
+
+
+
+
+function logged() {
+
+  let getIdUtente = localStorage.getItem('idUtente');
+
+  if (getIdUtente != null) {
+    console.log(getIdUtente);
+    user.classList.remove('d-none');
+    user.classList.add('d-block');
+    signup.classList.remove('d-none');
+    signup.classList.add('d-block');
+    logout.classList.remove('d-none');
+    logout.classList.add('d-block');
+    login.classList.add('d-none');
+
+  } else {
+    console.log(55);
+    user.classList.add('d-none');
+    signup.classList.add('d-none');
+    logout.classList.add('d-none');
+    login.classList.remove('d-none');
+    login.classList.add('d-block');
+  }
+
+  
+}
+
+
+function logOut() {
+  localStorage.removeItem('idUtente');
+  localStorage.removeItem('arrayIdOggetto');
+  localStorage.removeItem('arrayId');
+  localStorage.removeItem('totaleCarrello');
+  
+  let ruolo = localStorage.getItem('ruolo');
+  console.log(ruolo);
+  if (ruolo === "USER") {
+    console.log('si');
+    logged();
+  } else if (ruolo === "ADMIN") {
+    loggedAdmin();
+  }
+  localStorage.removeItem('ruolo');
+}
+
+logout.addEventListener('click', logOut);
+
+let arrayCarrello = [];
+let numeroArticoli = document.querySelector('#numeroArticoli');
+let numProdotti = 0;
+
+function contoCarrello() {
+  console.log(numProdotti);
+
+  arrayCarrello = JSON.parse(localStorage.getItem('arrayId'));
+  console.log(arrayCarrello);
+
+  if (arrayCarrello !== null) {
+
+    numProdotti = arrayCarrello.length;
+  }
+  console.log(numProdotti);
+  numeroArticoli.innerHTML = numProdotti;
+
+  if (numProdotti == 0) {
+    numeroArticoli.innerHTML = null;
+  }
+
+}
+contoCarrello();
+
+let ruolo = "";
+
+function ottieniRuolo() {
+
+  let idUtente = localStorage.getItem('idUtente');
+
+  if (idUtente !== null) {
+
+    fetch(`http://localhost:8080/api/utenti/${idUtente}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.ruolo);
+
+        ruolo = data.ruolo;
+        localStorage.setItem('ruolo', ruolo);
+
+
+        console.log(ruolo);
+        let ruoloLocale = localStorage.getItem('ruolo');
+        if (ruoloLocale === "USER") {
+          console.log('si');
+          logged();
+        } else if (ruoloLocale === "ADMIN") {
+          loggedAdmin();
+        }
+      });
+  }
+
+}
+ottieniRuolo();
+
+function loggedAdmin() {
+  let getIdUtente = localStorage.getItem('idUtente');
+
+  if (getIdUtente != null) {
+    console.log(getIdUtente);
+    user.classList.remove('d-none');
+    user.classList.add('d-block');
+    admin.classList.remove('d-none');
+    admin.classList.add('d-block');
+    logout.classList.remove('d-none');
+    logout.classList.add('d-block');
+    login.classList.add('d-none');
+
+  } else {
+    console.log(55);
+    user.classList.add('d-none');
+    logout.classList.add('d-none');
+    admin.classList.add('d-none');
+    login.classList.remove('d-none');
+    login.classList.add('d-block');
+  }
+
+}
