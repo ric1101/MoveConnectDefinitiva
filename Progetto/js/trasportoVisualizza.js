d17eb96101c39b413dd6755020d6471f08a933e5
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-10-17 16:50:01 +0200

    zioPorchetta

--- Progetto/js/trasportoVisualizza.js
+++ Progetto/js/trasportoVisualizza.js
@@ -1,26 +1,49 @@
 
 let bodyTabella = document.querySelector('.bodyTabella');
+console.log(bodyTabella);
 
 
-let URLB = ``;
+let URLB = `http://localhost:8080/api/richiesta/tutteLeRichieste`;
 fetch(URLB)
     .then((res) => res.json())
     .then((data) => {
 
+        recuperaDatiAzienda(data)
         console.log(data);
-        trasporto(data);
+        
 
     });
 
+function recuperaDatiAzienda(dati) {
 
-                              
+    dati.forEach(element => {
 
-function trasporto(dati) {
+
+        fetch(`http://localhost:8080/api/azienda/${element.azienda_id}`)
+            .then((res) => res.json())
+            .then((data) => {
+
+
+
+                trasporto(dati, data.logo, data.nomeAzienda);
+                console.log(data.logo, data.nomeAzienda);
+                
+                
+            });
+    });
+}
+
+
+
+
+function trasporto(dati, logo, azienda) {
 
     dati.forEach(element => {
 
 
         let tabella = `<tr>
+                        <td class="">${logo}</td>
+                        <td class="">${azienda}</td>
                         <td class="">${element.id}</td>
                         <td class="" data-eventoid="1">${element.comunePartenza}</td>
                         <td class="" data-eventoid="1">${element.comuneArrivo}</td>
@@ -28,7 +51,6 @@ function trasporto(dati) {
                         <td class="" data-eventoid="1">${element.tipoDiVeicolo}</td>
                         <td class="" data-eventoid="1">${element.carico}</td>
                         <td class="" data-eventoid="1">${element.scarico}</td>
-                        <td class="" data-eventoid="1">${element.azienda}</td>
                         <td class="" data-eventoid="1"><a class="btn btn-dark" href="">INFO</a></td>
                     </tr>`;
 
