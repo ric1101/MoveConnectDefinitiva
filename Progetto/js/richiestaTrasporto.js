de867ac597c8b5abda76ed73e21b1cb37e41bd24
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-10-18 16:27:25 +0200

    js post

--- Progetto/js/richiestaTrasporto.js
+++ Progetto/js/richiestaTrasporto.js
@@ -1,98 +1,124 @@
 
 
 
-let regioniPartenza = document.querySelector('.regioniPartenza');
-let provincePartenza = document.querySelector('.provincePartenza');
-let comuniPartenza = document.querySelector('.comuniPartenza');
-let indirizzoPartenza = document.querySelector('#indirizzoPartenza');
-let indirizzoDuePartenza = document.querySelector('#indirizzoDuePartenza');
-let capPartenza = document.querySelector('#capPartenza');
-let mq = document.querySelector('#mq');
-let veicolo = document.querySelector('#veicolo');
+let regionePartenza = document.querySelector('.regionePartenza');
+let provinciaPartenza = document.querySelector('.provinciaPartenza');
+let comunePartenza = document.querySelector('.comunePartenza');
+let indirizzoPartenza = document.querySelector('.indirizzoPartenza');
+let indirizzoDuePartenza = document.querySelector('.indirizzoDuePartenza');
+let capPartenza = document.querySelector('.capPartenza');
+let mq = document.querySelector('.mq');
+let tipoDiVeicolo = document.querySelector('.tipoDiVeicolo');
 
 
-let regioniArrivo = document.querySelector('.regioniArrivo');
-let provinceArrivo = document.querySelector('.provinceArrivo');
-let comuniArrivo = document.querySelector('.comuniArrivo');
-let indirizzoArrivo = document.querySelector('#indirizzoArrivo');
-let indirizzoDueArrivo = document.querySelector('#indirizzoDueArrivo');
-let capArrivo = document.querySelector('#capArrivo');
+let regioneArrivo = document.querySelector('.regioneArrivo');
+let provinciaArrivo = document.querySelector('.provinciaArrivo');
+let comuneArrivo = document.querySelector('.comuneArrivo');
+let indirizzoArrivo = document.querySelector('.indirizzoArrivo');
+let indirizzoDueArrivo = document.querySelector('.indirizzoDueArrivo');
+let capArrivo = document.querySelector('.capArrivo');
 
 
-let carico = document.querySelector('#carico');
-let scarico = document.querySelector('#scarico');
-let note = document.querySelector('#note');
+let carico = document.querySelector('.carico');
+let scarico = document.querySelector('.scarico');
+let note = document.querySelector('.note');
 
-let btnInvioRichiestaTrasporto = document.querySelector('#btnInvioRichiestaTrasporto');
+let btnInvioRichiestaTrasporto = document.querySelector('.btnInvioRichiestaTrasporto');
 
 class Trasporto {
-    constructor(regioniPartenza,
-        provincePartenza,
-        comuniPartenza,
+    constructor(regionePartenza,
+        provinciaPartenza,
+        comunePartenza,
         indirizzoPartenza,
         indirizzoDuePartenza,
         capPartenza,
         mq,
-        veicolo,
-        regioniArrivo,
-        provinceArrivo,
-        comuniArrivo,
+        tipoDiVeicolo,
+        regioneArrivo,
+        provinciaArrivo,
+        comuneArrivo,
         indirizzoArrivo,
         indirizzoDueArrivo,
         capArrivo,
         carico,
         scarico,
-        note) {
+        note,
+        azienda_id) {
 
-        (this.regioniPartenza = regioniPartenza),
-            (this.provincePartenza = provincePartenza),
-            (this.comuniPartenza = comuniPartenza),
+        (this.regionePartenza = regionePartenza),
+            (this.provinciaPartenza = provinciaPartenza),
+            (this.comunePartenza = comunePartenza),
             (this.indirizzoPartenza = indirizzoPartenza),
             (this.indirizzoDuePartenza = indirizzoDuePartenza),
             (this.capPartenza = capPartenza),
             (this.mq = mq),
-            (this.veicolo = veicolo),
-            (this.regioniArrivo = regioniArrivo),
-            (this.provinceArrivo = provinceArrivo),
-            (this.comuniArrivo = comuniArrivo),
+            (this.tipoDiVeicolo = tipoDiVeicolo),
+            (this.regioneArrivo = regioneArrivo),
+            (this.provinciaArrivo = provinciaArrivo),
+            (this.comuneArrivo = comuneArrivo),
             (this.indirizzoArrivo = indirizzoArrivo),
             (this.indirizzoDueArrivo = indirizzoDueArrivo),
             (this.capArrivo = capArrivo),
             (this.carico = carico),
             (this.scarico = scarico),
-            (this.note = note)
+            (this.note = note),
+            (this.azienda_id = azienda_id)
     }
 }
 
 
 function inviaRichiesta() {
 
+    let azienda_id = localStorage.getItem('idUtente');
+    console.log(azienda_id);
+
+
     let nuovaRichiestaTrasporto = new Trasporto(
-        regioniPartenza.value,
-        provincePartenza.value,
-        comuniPartenza.value,
+        regionePartenza.value,
+        provinciaPartenza.value,
+        comunePartenza.value,
         indirizzoPartenza.value,
         indirizzoDuePartenza.value,
         capPartenza.value,
         mq.value,
-        veicolo.value,
-        regioniArrivo.value,
-        provinceArrivo.value,
-        comuniArrivo.value,
+        tipoDiVeicolo.value,
+        regioneArrivo.value,
+        provinciaArrivo.value,
+        comuneArrivo.value,
         indirizzoArrivo.value,
         indirizzoDueArrivo.value,
         capArrivo.value,
         carico.value,
         scarico.value,
-        note.value
+        note.value,
+        azienda_id
     );
 
+    // console.log(regionePartenza.value,);
+    // console.log(provinciaPartenza.value,);
+    // console.log(comunePartenza.value,);
+    // console.log(indirizzoPartenza.value,);
+    // console.log(indirizzoDuePartenza.value,);
+    // console.log(capPartenza.value,);
+    // console.log(mq.value,);
+    // console.log(veicolo.value,);
+    // console.log(regioneArrivo.value,);
+    // console.log(provinciaArrivo.value,);
+    // console.log(comuneArrivo.value,);
+    // console.log(indirizzoArrivo.value,);
+    // console.log(indirizzoDueArrivo.value,);
+    // console.log(capArrivo.value,);
+    // console.log(carico.value,);
+    // console.log(scarico.value,);
+    // console.log(note.value);
     
-    let idAzienda = localStorage.getItem('idUtente');
-    console.log(idAzienda);
+    console.log(nuovaRichiestaTrasporto);
+    
 
 
-    fetch(`http://localhost:8080/api/richiesta/nuovaRichiestaTrasporto/${idAzienda}`, {
+    
+
+    fetch(`http://localhost:8080/api/azienda/nuovaRichiestaTrasporto/${azienda_id}`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -101,35 +127,55 @@ function inviaRichiesta() {
 
     })
 
+    location.reload();
+
+    
+
 
 }
+
+// function controllaValiditaCampi() {
+    
+//controlli da fare qua dentro
+//capPartenza
+//capArrivo
+//mq
+//carico
+//scarico
+
+
+
+// }
+
 
 
 function checkCampi() {
-    
-    if(
-    regioniPartenza.value.trim() != "" &&
-    provincePartenza.value.trim() != "" &&
-    comuniPartenza.value.trim() != "" &&
-    indirizzoPartenza.value.trim() != "" &&
-    indirizzoDuePartenza.value.trim() != "" &&
-    capPartenza.value.trim() != "" &&
-    mq.value.trim() != "" &&
-    veicolo.value.trim() != "" &&
-    regioniArrivo.value.trim() != "" &&
-    provinceArrivo.value.trim() != "" &&
-    comuniArrivo.value.trim() != "" &&
-    indirizzoArrivo.value.trim() != "" &&
-    indirizzoDueArrivo.value.trim() != "" &&
-    capArrivo.value.trim() != "" &&
-    carico.value.trim() != "" &&
-    scarico.value.trim() != "" &&
-    note.value
-    ){ }
 
+    event.preventDefault();
+    if (regionePartenza.value.trim() != "" &&
+        provinciaPartenza.value.trim() != "" &&
+        comunePartenza.value.trim() != "" &&
+        indirizzoPartenza.value.trim() != "" &&
+        capPartenza.value.trim() != "" &&
+        mq.value.trim() != "" &&
+        tipoDiVeicolo.value.trim() != "" &&
+        regioneArrivo.value.trim() != "" &&
+        provinciaArrivo.value.trim() != "" &&
+        comuneArrivo.value.trim() != "" &&
+        indirizzoArrivo.value.trim() != "" &&
+        capArrivo.value.trim() != "" &&
+        carico.value.trim() != "" &&
+        scarico.value.trim() != "") {
+
+        // controllaValiditaCampi();
+        inviaRichiesta();
+
+    }
 
 
 }
 
 
+
 btnInvioRichiestaTrasporto.addEventListener('click', checkCampi);
+
