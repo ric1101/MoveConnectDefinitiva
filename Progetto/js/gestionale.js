85518c2b4759bc37120eb4b4d5a99a98040919cb
ric1101 <161829770+ric1101@users.noreply.github.com>
2025-03-27 14:26:51 +0100

    push vetusto

--- Progetto/js/gestionale.js
+++ Progetto/js/gestionale.js
@@ -16,14 +16,14 @@ function popolaGestionale() {
 
                 let articolo = `<tr>
 
-                        <td class="text-center"><textarea name="" id="">${element.genere}</textarea></td>
+                        <td class="text-center"><textarea name="" id="" style="width: 100%;">${element.genere}</textarea></td>
                         <td class="text-center">${element.id}</td>
-                        <td class="text-center" data-eventoid="1"><textarea name="" id="">${element.titolo}</textarea></td>
-                        <td class="text-center" data-eventoid="1"><textarea name="" id="">${element.desc}</textarea></td>
-                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="height: 100%; width: 100%;">${element.testo}</textarea></td>
+                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="width: 100%;">${element.titolo}</textarea></td>
+                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="width: 100%;">${element.desc}</textarea></td>
+                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="width: 100%;">${element.testo}</textarea></td>
                         <td class="text-center" data-eventoid="1"><img src="${element.img}" alt="" style="width: 100px; height: 100px;"></img></td>
-                        <td class="text-center" data-eventoid="1"><textarea name="" id="">${element.data}</textarea></td>
-                        <td class="text-center" data-eventoid="1"><textarea name="" id="">${element.writer}</textarea></td>
+                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="width: 100%;">${element.data}</textarea></td>
+                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="width: 100%;">${element.writer}</textarea></td>
                         <td class="text-center" data-eventoid="1"><a class="btn btn-primary" onclick="eliminaBlog(${element.id})"><i class="fa-solid fa-pen-to-square"></i></a></td>
                         <td class="text-center" data-eventoid="1"><a class="btn btn-danger" onclick="modificaBlog(${element.id}, )"><i class="fa-solid fa-trash-can"></i></a></td>
 
