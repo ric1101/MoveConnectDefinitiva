90f3d69f1d94e4bd6adf4324ee8e626f2af17f09
ric1101 <161829770+ric1101@users.noreply.github.com>
2025-03-27 16:07:51 +0100

    trenbolone

--- Progetto/js/gestionale.js
+++ Progetto/js/gestionale.js
@@ -16,16 +16,16 @@ function popolaGestionale() {
 
                 let articolo = `<tr>
 
-                        <td class="text-center"><textarea name="" id="" style="width: 100%;">${element.genere}</textarea></td>
+                        <td class="text-center">${element.genere}</td>
                         <td class="text-center">${element.id}</td>
-                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="width: 100%;">${element.titolo}</textarea></td>
-                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="width: 100%;">${element.desc}</textarea></td>
-                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="width: 100%;">${element.testo}</textarea></td>
+                        <td class="text-center" data-eventoid="1">${element.titolo}</td>
+                        <td class="text-center" data-eventoid="1">${element.desc}</td>
+                        <td class="text-center" data-eventoid="1">${element.testo}</td>
                         <td class="text-center" data-eventoid="1"><img src="${element.img}" alt="" style="width: 100px; height: 100px;"></img></td>
-                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="width: 100%;">${element.data}</textarea></td>
-                        <td class="text-center" data-eventoid="1"><textarea name="" id="" style="width: 100%;">${element.writer}</textarea></td>
+                        <td class="text-center" data-eventoid="1">${element.data}</td>
+                        <td class="text-center" data-eventoid="1">${element.writer}</td>
                         <td class="text-center" data-eventoid="1"><a class="btn btn-primary" onclick="eliminaBlog(${element.id})"><i class="fa-solid fa-pen-to-square"></i></a></td>
-                        <td class="text-center" data-eventoid="1"><a class="btn btn-danger" onclick="modificaBlog(${element.id}, )"><i class="fa-solid fa-trash-can"></i></a></td>
+                        <td class="text-center" data-eventoid="1"><a class="btn btn-danger" onclick="modificaBlog(${element.id})"><i class="fa-solid fa-trash-can"></i></a></td>
 
                     </tr>`;
 
@@ -42,11 +42,15 @@ popolaGestionale();
 
 
 
-
 function eliminaBlog(id) {
 
+    fetch(`http://127.0.0.1:8080/api/blog/eliminaBlog/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        }
 
-
+    })
 
 }
 
@@ -54,6 +58,34 @@ function eliminaBlog(id) {
 function modificaBlog(id) {
 
 
+    // class NuovoArticolo {
+    //     constructor(titolo, desc, testo, img, data, writer, genere) {
+    //         (this.titolo = titolo),
+    //         (this.desc = desc),
+    //         (this.testo = testo),
+    //         (this.img = img),
+    //         (this.data = data),
+    //         (this.writer = writer),
+    //         (this.genere = genere)
+    //     }
+    // }
 
+    // let nuovoArticolo = new NuovoArticolo(
+
+
+    // );
+
+
+    // fetch(`http://127.0.0.1:8080/api/blog/modificaBlog/${id}`, {
+    //     method: "PUT",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(),
+
+    // })
+
+    localStorage.setItem('idModifica', id);
+    window.location.href = 'modificaBlog.html';
 
 }
---
+++ Progetto/js/modificaBlog.js
@@ -0,0 +1,36 @@
+
+
+let genere = document.querySelector('.genere');
+let titolo = document.querySelector('.titolo');
+let desc = document.querySelector('.desc');
+let testo = document.querySelector('.testo');
+let img = document.querySelector('.img');
+let datato = document.querySelector('.data');
+let writer = document.querySelector('.writer');
+
+
+
+
+function popolaModBlog() {
+
+    let idMod = localStorage.getItem('idModifica');
+
+    fetch(`http://127.0.0.1:8080/api/blog/${idMod}`)
+        .then((res) => res.json())
+        .then((data) => {
+
+            titolo.value = data.titolo;
+            desc.value = data.desc;
+            testo.value = data.testo;
+            img.value = data.img;
+            datato.value = data.data;
+            writer.value = data.writer;
+
+        });
+
+
+}
+popolaModBlog();
+
+
+
