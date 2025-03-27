60b1a265571ef41c2a1483c39d55a0345ed0d45f
ric1101 <161829770+ric1101@users.noreply.github.com>
2025-03-27 12:45:17 +0100

    gang

--- Progetto/js/gestionale.js
+++ Progetto/js/gestionale.js
@@ -14,19 +14,20 @@ function popolaGestionale() {
             data.forEach(element => {
 
 
-
                 let articolo = `<tr>
 
-                                <td class="text-center">${element.genere}</td>
-                                <td class="text-center">${element.id}</td>
-                                <td class="text-center" data-eventoid="1">${element.titolo}</td>
-                                <td class="text-center" data-eventoid="1">${element.desc}</td>
-                                <td class="text-center" data-eventoid="1">${element.testo}</td>
-                                <td class="text-center" data-eventoid="1"><img src="${element.img}" alt="" style="width: 100px; height: 100px;"></img></td>
-                                <td class="text-center" data-eventoid="1">${element.data}</td>
-                                <td class="text-center" data-eventoid="1">${element.writer}</td>
+                        <td class="text-center"><input type="text">${element.genere}</input></td>
+                        <td class="text-center">${element.id}</td>
+                        <td class="text-center" data-eventoid="1"><input type="text">${element.titolo}</input></td>
+                        <td class="text-center" data-eventoid="1"><input type="text">${element.desc}</input></td>
+                        <td class="text-center" data-eventoid="1"><input type="text">${element.testo}</input></td>
+                        <td class="text-center" data-eventoid="1"><img src="${element.img}" alt="" style="width: 100px; height: 100px;"></img></td>
+                        <td class="text-center" data-eventoid="1"><input type="text">${element.data}</input></td>
+                        <td class="text-center" data-eventoid="1"><input type="text">${element.writer}</input></td>
+                        <td class="text-center" data-eventoid="1"><a class="btn btn-primary" onclick="eliminaBlog(${element.id})"><i class="fa-solid fa-pen-to-square"></i></a></td>
+                        <td class="text-center" data-eventoid="1"><a class="btn btn-danger" onclick="modificaBlog(${element.id}, )"><i class="fa-solid fa-trash-can"></i></a></td>
 
-                            </tr>`;
+                    </tr>`;
 
 
                 bodyTabella.innerHTML += articolo;
@@ -35,8 +36,24 @@ function popolaGestionale() {
 
         });
 
+}
+
+popolaGestionale();
+
+
+
+
+function eliminaBlog(id) {
+
+
+
 
 }
 
 
-popolaGestionale();
+function modificaBlog(id) {
+
+
+
+
+}
