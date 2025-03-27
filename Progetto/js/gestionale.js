b1f8fc9a3ce0be23672048b513581d7d3f25ee71
ric1101 <161829770+ric1101@users.noreply.github.com>
2025-03-27 16:10:55 +0100

    marameo

--- Progetto/js/gestionale.js
+++ Progetto/js/gestionale.js
@@ -24,8 +24,8 @@ function popolaGestionale() {
                         <td class="text-center" data-eventoid="1"><img src="${element.img}" alt="" style="width: 100px; height: 100px;"></img></td>
                         <td class="text-center" data-eventoid="1">${element.data}</td>
                         <td class="text-center" data-eventoid="1">${element.writer}</td>
-                        <td class="text-center" data-eventoid="1"><a class="btn btn-primary" onclick="eliminaBlog(${element.id})"><i class="fa-solid fa-pen-to-square"></i></a></td>
-                        <td class="text-center" data-eventoid="1"><a class="btn btn-danger" onclick="modificaBlog(${element.id})"><i class="fa-solid fa-trash-can"></i></a></td>
+                        <td class="text-center" data-eventoid="1"><a class="btn btn-primary" onclick="modificaBlog(${element.id})"><i class="fa-solid fa-pen-to-square"></i></a></td>
+                        <td class="text-center" data-eventoid="1"><a class="btn btn-danger" onclick="eliminaBlog(${element.id})"><i class="fa-solid fa-trash-can"></i></a></td>
 
                     </tr>`;
 
